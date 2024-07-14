b56eadd5387dd2ba70c672399da99d776867fe9d
Mhayk Whandson <hi@mhayk.com>
2024-07-14 12:16:18 +0100

    feat: solving concurrency issue using atomic sync/atomic package

---
+++ Multithreading/3.2-concurrency-in-real-world-atomic/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+	"time"
+)
+
+var number uint64 = 0
+
+func main() {
+	// m := sync.Mutex{}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// m.Lock()
+		// number++
+		atomic.AddUint64(&number, 1)
+		// m.Unlock()
+		time.Sleep(300 * time.Millisecond)
+		w.Write([]byte(fmt.Sprintf("You are the visitor number %d", number)))
+	})
+
+	http.ListenAndServe(":3000", nil)
+}
